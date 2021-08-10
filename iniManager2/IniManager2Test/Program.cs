4f573fd19b9080fdb20bbb1b97c3fd7969fee356
guigui55 <guigui55@outlook.jp>
2021-08-10 22:23:28 +0900

    Add IniManager Solution

---
+++ iniManager2/IniManager2Test/Program.cs
@@ -0,0 +1,227 @@
+﻿using ErrorManager;
+using IniManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniManager2Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test03(args);
+            Console.ReadKey();
+        }
+
+        static void Test03(string[] args)
+        {
+            try
+            {
+                // iniファイルの内容が空の時、終了しない
+                ErrorManager.ErrorManager _error = new ErrorManager.ErrorManager(1);
+                string iniPath = System.IO.Directory.GetCurrentDirectory() + @"\settings.ini";
+                _error.AddLog("iniPath = " + iniPath);
+                if (args.Length > 0)
+                {
+                    iniPath = args[0];
+                }
+                int ret;
+                IniManager.IniManager manager = new IniManager.IniManager(_error);
+
+                // Ini Path
+                // フォーマットはなしにする、機能としては残してある
+                //string FormatPath = System.IO.Directory.GetCurrentDirectory() + @"\iniFormat.txt";
+                // フォーマットを作成 -> プロジェクトにインポートしておく
+                //string iniFormatString = new Common().ExcuteRead(FormatPath);
+
+                // Path をセットする、なければ作る
+                _ = manager.SetPathAndCreateFileIfPathNotExists(iniPath, "");
+                //if (ret < 1) { manager.ShowErrorMessages(); return; }
+                if (manager._error.HasException()) { manager.ShowErrorMessages(); manager._error.ClearError(); }
+
+                // あったら読み込む
+                ret = manager.ReadIni();
+                if (ret < 1) { manager.ShowErrorMessages(); return; }
+
+                // 読み込んだら値をクラス変数にセット
+                ret = manager.SetSectionFromPath();
+                if (ret == 0) { manager.ShowErrorMessages(); return; }
+
+
+                string buf;
+                string section = "TestSection";
+                string param = "TestProperty1";
+                // 値を読み込む
+                buf = manager.GetParamterValueWhenAddValueIsNothing(section, param);
+                if (manager._error.HasException())
+                {
+                    Console.WriteLine("ERROR : " + manager.GetErrorMessages());
+                }
+                // テスト出力
+                Console.WriteLine("GetParametersValue : [" + section + "] " + param + " = " + buf);
+
+                // 値をクラスへ保存する
+                string value = "TestProperty1-2";
+                 manager.SetParamterValueWhenAddValueIsNothing(section, param, value);
+                if (_error.HasException()) { Console.WriteLine("* ERROR : " + manager.GetErrorMessages()); }
+                Console.WriteLine("WetParameter : [" + section + "] " + param + " = " + buf + " ,value="+value);
+
+                // 値を読み込む
+                buf = manager.GetParamterValueWhenAddValueIsNothing(section, param);
+                if (manager._error.HasException()) { Console.WriteLine("* ERROR : " + manager.GetErrorMessages()); }
+                // テスト出力
+                Console.WriteLine("GetParametersValue : [" + section + "] " + param + " = " + buf);
+
+                // クラスで保持している値をすべて書き込む
+                ret = manager.WriteAllData();
+                if (manager._error.HasException()) { Console.WriteLine("* ERROR : " + manager.GetErrorMessages()); }
+
+                // 結果出力
+                if (ret > 0) { Console.WriteLine("Success."); }
+                else { Console.WriteLine("* Error WriteAllData Failed."); }
+                Console.ReadKey();
+            } catch (Exception ex)
+            {
+
+            }
+        }
+
+        static void Test02(string[] args)
+        {
+            try
+            {
+                // iniファイルの内容が空の時、終了しない
+                ErrorManager.ErrorManager _error = new ErrorManager.ErrorManager(1);
+                string iniPath = System.IO.Directory.GetCurrentDirectory() + @"\settings.ini";
+                _error.AddLog("iniPath = " + iniPath);
+                if (args.Length > 0)
+                {
+                    iniPath = args[0];
+                }
+                int ret;
+                IniManager.IniManager manager = new IniManager.IniManager(_error);
+
+                // Ini Path
+                // フォーマットはなしにする、機能としては残してある
+                //string FormatPath = System.IO.Directory.GetCurrentDirectory() + @"\iniFormat.txt";
+                // フォーマットを作成 -> プロジェクトにインポートしておく
+                //string iniFormatString = new Common().ExcuteRead(FormatPath);
+
+                // Path をセットする、なければ作る
+                _ = manager.SetPathAndCreateFileIfPathNotExists(iniPath, "");
+                //if (ret < 1) { manager.ShowErrorMessages(); return; }
+                if (manager._error.HasException()) { manager.ShowErrorMessages(); manager._error.ClearError(); }
+
+                // あったら読み込む
+                ret = manager.ReadIni();
+                if (ret < 1) { manager.ShowErrorMessages(); return; }
+
+                // 読み込んだら値をクラス変数にセット
+                ret = manager.SetSectionFromPath();
+                if (ret == 0) { manager.ShowErrorMessages(); return; }
+                
+
+                string buf;
+                string section = "TestSection";
+                string param = "TestProperty1";
+                // 値を読み込む
+                buf = manager.GetParameterValue(section, param);
+                if (manager._error.HasException()) { 
+                    Console.WriteLine("ERROR : " + manager.GetErrorMessages());  
+                }
+                // テスト出力
+                Console.WriteLine("GetParametersValue : [" + section + "] " + param + " = " + buf);
+
+                // 値をクラスへ保存する
+                string value = "TestProperty1-2";
+                ret = manager.SetParameterValue(section, param, value);
+                if (ret < 1) { Console.WriteLine("ERROR : " + manager.GetErrorMessages()); }
+
+                // 値を読み込む
+                buf = manager.GetParameterValue(section, param);
+                if (manager._error.HasException()) { Console.WriteLine("ERROR : " + manager.GetErrorMessages()); }
+                // テスト出力
+                Console.WriteLine("GetParametersValue : [" + section + "] " + param + " = " + buf);
+
+                // クラスで保持している値をすべて書き込む
+                ret = manager.WriteAllData();
+                if (manager._error.HasException()) { Console.WriteLine("ERROR : " + manager.GetErrorMessages()); }
+
+                // 結果出力
+                if (ret > 0) { Console.WriteLine("Success."); }
+                else { Console.WriteLine("WriteAllData Failed."); }
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.ReadKey();
+            }
+
+        }
+
+        static void Test01(string[] args)
+        {
+            // iniファイルの内容が空の時、終了する
+            ErrorManager.ErrorManager _error = new ErrorManager.ErrorManager(1);
+            string iniPath = System.IO.Directory.GetCurrentDirectory() + @"\settings.ini";
+            _error.AddLog("iniPath = " + iniPath);
+            if (args.Length > 0)
+            {
+                iniPath = args[0];
+            }
+            int ret;
+            IniManager.IniManager manager = new IniManager.IniManager(_error);
+
+            // Ini Path
+            // フォーマットはなしにする、機能としては残してある
+            //string FormatPath = System.IO.Directory.GetCurrentDirectory() + @"\iniFormat.txt";
+            // フォーマットを作成 -> プロジェクトにインポートしておく
+            //string iniFormatString = new Common().ExcuteRead(FormatPath);
+
+            // Path をセットする、なければ作る
+            _ = manager.SetPathAndCreateFileIfPathNotExists(iniPath, "");
+            //if (ret < 1) { manager.ShowErrorMessages(); return; }
+            if (manager._error.HasException()) { manager.ShowErrorMessages(); manager._error.ClearError(); }
+
+            // あったら読み込む
+            ret = manager.ReadIni();
+            if (ret < 1) { manager.ShowErrorMessages(); return; }
+
+            // 読み込んだら値をクラス変数にセット
+            ret = manager.SetSectionFromPath();
+            if (ret < 1) { manager.ShowErrorMessages(); return; }
+
+            string buf;
+            string section = "TestSection";
+            string param = "TestProperty1";
+            // 値を読み込む
+            buf = manager.GetParameterValue(section, param);
+            if (manager._error.HasException()) { manager.ShowErrorMessages(); return; }
+            // テスト出力
+            Console.WriteLine("GetParametersValue : [" + section + "] " + param + " = " + buf);
+
+            // 値をクラスへ保存する
+            string value = "TestProperty1-2";
+            ret = manager.SetParameterValue(section, param, value);
+            if (ret < 1) { manager.ShowErrorMessages(); return; }
+
+            // 値を読み込む
+            buf = manager.GetParameterValue(section, param);
+            if (manager._error.HasException()) { manager.ShowErrorMessages(); return; }
+            // テスト出力
+            Console.WriteLine("GetParametersValue : [" + section + "] " + param + " = " + buf);
+
+            // クラスで保持している値をすべて書き込む
+            ret = manager.WriteAllData();
+            if (manager._error.HasException()) { manager.ShowErrorMessages(); return; }
+
+            // 結果出力
+            if (ret > 0) { Console.WriteLine("Success."); }
+            else { Console.WriteLine("Failed."); }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ iniManager2/iniManager2/Error.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace IniManager
+{
+    class Error
+    {
+        public  int DebugMode = 1;
+        public string ErrorMessageToUser = "";
+        public Exception LastException = null;
+        public string LastExceptionMessage = "";
+        public string LastErrorMessage = "";
+
+        /// <summary>
+        ///  コンストラクタ。引数は 1 ならエラー表示時に Debug.Writeline で出力する。
+        /// </summary>
+        public Error(int debugMode)
+        {
+            DebugMode = debugMode;
+        }
+
+        /// <summary>
+        ///  最後に発生した例外の Message を取得する
+        /// </summary>
+        public string GetExceptionMessage() { 
+            if (LastException is null) { return ""; }
+            return LastException.Message;  
+        }
+
+        /// <summary>
+        ///  最後に例外が発生したときの LastErrorMessage を取得する
+        /// </summary>
+        public string GetErrorMessage() { return LastErrorMessage; }
+
+        /// <summary>
+        ///  最後に例外が発生したときの LastErrorMessage と 例外の Message を取得する
+        /// </summary>
+        public string GetMessages() { return GetErrorMessage() + "\n" + GetExceptionMessage(); }
+
+
+        /// <summary>
+        ///  例外情報を追記する
+        /// </summary>
+        public void AddException(Exception ex,string msg)
+        {
+            if (!(ex is null))
+            {
+                LastExceptionMessage += "\n" +ex.Message;
+            }
+            LastErrorMessage += "\n" + msg;
+        }
+
+        /// <summary>
+        ///  例外情報を保存する
+        /// </summary>
+        public void SaveException(Exception ex,string msg)
+        {
+            LastException = ex;
+            LastExceptionMessage = ex.Message;
+            LastErrorMessage = msg;
+            if (DebugMode == 1)
+            {
+                Debug.WriteLine(msg);
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        ///  例外が発生しているか
+        /// </summary>
+        public bool HasException()
+        {
+            if (!(LastException == null)) { return true; }
+            return false;
+        }
+
+        /// <summary>
+        ///  保持している例外情報をクリアする
+        /// </summary>
+        public void ClearError()
+        {
+            LastErrorMessage = "";
+            LastException = null;
+        }
+    }
+}
