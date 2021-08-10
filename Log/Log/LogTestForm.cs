e77efd1b825dd3715e21314cb48f9decce4f3eaf
guigui55 <guigui55@outlook.jp>
2021-08-10 22:20:20 +0900

    Add Log Solution

---
+++ Log/Log/LogTestForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Log
+{
+    public partial class LogTest : Form
+    {
+        LogManager logManager;
+        public LogTest()
+        {
+            InitializeComponent();
+        }
+
+        private void LogTest_Load(object sender, EventArgs e)
+        {
+            string folderPath = System.IO.Directory.GetCurrentDirectory();
+            string filePath = "Log.Log";
+            string path = folderPath + "\\" + filePath;
+            logManager = new LogManager(1, path);
+
+            logManager.Add("log value 1","log notes");
+            logManager.Add("log value 2", "");
+            logManager.Add(1, 2, "log value 3", "");
+
+            Console.WriteLine("-----------\n"+logManager.GetLogDataListAtString());
+        }
+    }
+}
